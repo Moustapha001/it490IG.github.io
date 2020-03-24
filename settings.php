eb9e5430994aac01aa31c8ca4bac2163f3fa8479
Moustapha001 <60150830+Moustapha001@users.noreply.github.com>
2020-03-24 12:17:57 -0400

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+   
+   $settings = array(
+         "ClientID" => "853404941837918",
+		 "ClientSecret" => "ba5748aa8ac8acc658d1417e4b06e5ae",
+		 "redirectURL" => "https://moustapha001.github.io/it490IG.github.io/Login.html"  
+ 
+                   );
+?>

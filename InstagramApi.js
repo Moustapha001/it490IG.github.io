2406d6d96748f4878e2a8cf36c21d9fbd80b9c96
Moustapha001 <60150830+Moustapha001@users.noreply.github.com>
2020-03-23 20:49:51 -0400

    Add files via upload

---
+++ InstagramApi.js
@@ -0,0 +1,12 @@
+<script>
+
+fetch('https://moustapha001.github.io/it490IG.github.io/Login.html')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+
+</script>

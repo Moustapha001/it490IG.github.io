1dab10431c5135cfde32e5df6319512b133d6858
Moustapha001 <60150830+Moustapha001@users.noreply.github.com>
2020-03-23 21:04:38 -0400

    Update InstagramApi.js

--- InstagramApi.js
+++ InstagramApi.js
@@ -1,6 +1,11 @@
 <script>
 
-fetch('https://moustapha001.github.io/it490IG.github.io/Login.html')
+fetch('https://api.instagram.com/oauth/authorize
+  ?client_id=226838558508870
+  &redirect_uri=//https:moustapha001.github.io/it490IG.github.io/Login.html
+  &scope=user_profile,user_media
+  &response_type=code')
+      
   .then((response) => {
     return response.json();
   })

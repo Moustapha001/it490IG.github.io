ed7f511aa6582cfb4a1f4b7dd384c568841c8f3d
Moustapha001 <60150830+Moustapha001@users.noreply.github.com>
2020-03-23 22:01:09 -0400

    Update InstagramApi.js

--- InstagramApi.js
+++ InstagramApi.js
@@ -1,24 +1,7 @@
-var unirest = require("unirest");
-
-var req = unirest("GET", "https://api.instagram.com/oauth/authorize
-  ?client_id=853404941837918
-  &redirect_uri=//https:moustapha001.github.io/it490IG.github.io/Login.html
-  &scope=user_profile,user_media
-  &response_type=code");
-
-req.query({
-	"kullaniciadi": "nasa",
-	"lang": "en"
-});
-
-req.headers({
-	"x-rapidapi-host": "https://moustapha001.github.io/it490IG.github.io/Login.html",
-	"x-rapidapi-key": "IGQVJYV0cyR0pGNWVUTDhxQmd1VHBiMnlleFNYNV9xNW9xUW9BX1NUSTd3QmxIbXFOeE1lR2poS0tJS01RbFlnM1R5QzdCUnlrdGlwUkJRb3o1RE50aWxEYXhFdVRyN3dwUU9ISnBOelJLVk9rcVU0SQZDZD"
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
+fetch('https://api.instagram.com/v1/self/media/recent?access_token=IGQVJYV0cyR0pGNWVUTDhxQmd1VHBiMnlleFNYNV9xNW9xUW9BX1NUSTd3QmxIbXFOeE1lR2poS0tJS01RbFlnM1R5QzdCUnlrdGlwUkJRb3o1RE50aWxEYXhFdVRyN3dwUU9ISnBOelJLVk9rcVU0SQZDZD')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });

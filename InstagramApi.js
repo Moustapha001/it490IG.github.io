b4f85c3b43bea92c18aa749f387b906930577080
Moustapha001 <60150830+Moustapha001@users.noreply.github.com>
2020-03-24 12:35:37 -0400

    Update and rename InstagramApi.js to InstagramApi.php

--- InstagramApi.js
+++
@@ -1,7 +0,0 @@
-fetch('https://api.instagram.com/v1/self/media/recent?access_token=IGQVJYV0cyR0pGNWVUTDhxQmd1VHBiMnlleFNYNV9xNW9xUW9BX1NUSTd3QmxIbXFOeE1lR2poS0tJS01RbFlnM1R5QzdCUnlrdGlwUkJRb3o1RE50aWxEYXhFdVRyN3dwUU9ISnBOelJLVk9rcVU0SQZDZD')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });

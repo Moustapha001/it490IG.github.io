72cd857e7d28dd66f1010a3e7de6641419f8e694
Moustapha001 <60150830+Moustapha001@users.noreply.github.com>
2020-04-23 21:30:06 -0400

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,8 +1,7 @@
 <?php
    
   
-	define( 'CLIENT_ID', '853404941837918');
-	define( 'CLIENT_SECRET', 'ba5748aa8ac8acc658d1417e4b06e5ae');
-	define( 'REDIRECT_URI', 'https://moustapha001.github.io/it490IG.github.io/Login.html' );
-	define( 'ACCESS_TOKEN', 'IGQVJXZAmRsdktrSkJRS1JSX29yd1hfdTMwZAUg5TmYwSHJpOWVUZAjVNY3BhWkxQeGZACV3RRTmF1X1ZAmZAndsV3VwWDNvaUJMamx2dE9HbHJCZAWNHVndVNGdLQmQ4UHowdUZASSFJoMXpMSm9PR2VwQU40eAZDZD' );
-?>
+	define( 'IGAppID', '853404941837918');
+	define( 'IGAppSECRET', 'ba5748aa8ac8acc658d1417e4b06e5ae');
+	define( 'REDIRECT_URI', 'https://moustapha001.github.io/it490IG.github.io/Index.html' );
+	define( 'ACCESS_TOKEN', 'IGQVJYdEdwSzZAiZA2lQNktjNzMyMUtvNmZACY0JSTWVpZAXUwdVYzWHVTc1NINW5GU2NEaDBkekJEcnU2MHRhMWszZAGYwZAkxjN20zekZASeEZAkUGtwakIzUjNGMm5zN3NyZATJ4RXhldUJjTURDaVpCcEFSRgZDZD');
